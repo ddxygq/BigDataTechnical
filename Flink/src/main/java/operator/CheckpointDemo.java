ded899043b0d9ba3dfb416f5256eee39fe8bef37
keguang <keguang@flash.cn>
2023-12-29 17:41:10 +0800

    checkpoint/savepoint

---
+++ Flink/src/main/java/operator/CheckpointDemo.java
@@ -0,0 +1,24 @@
+package operator;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class CheckpointDemo {
+
+    public static void main(String[] args) {
+        StreamExecutionEnvironment senv = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // checkpoint间隔，这里配置为10s进行一次，单位是 ms
+        senv.enableCheckpointing(10 * 1000);
+
+        // checkpoint超时时间，单位ms，超过将被丢弃
+        senv.getCheckpointConfig().setCheckpointTimeout(5 * 1000);
+
+        // 两个checkpoint的间隔，不是一个完了，下一个立马开始
+        senv.getCheckpointConfig().setCheckpointInterval(1000);
+
+        // 配置checkpoint目录
+        senv.getCheckpointConfig().setCheckpointStorage("hdfs:///flink/checkpoints/jobname");
+
+
+    }
+}
